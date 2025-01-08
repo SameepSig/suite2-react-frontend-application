722bb6b2b0a3e25747a62a1380eba83496e09fdc
SameepSig <sameepsigdel@gmail.com>
2025-01-08 14:33:14 +0545

    Reliably load homepage when there is no data at all

--- src/elements/Home.jsx
+++ src/elements/Home.jsx
@@ -1,66 +1,89 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-    const [data, setData] = useState([])
-    const [deleted, setDeleted] = useState(true)
-    useEffect(()=>{
-        if(deleted){
-            setDeleted(false)
-        axios.get('/students')
-        .then((res)=>{
-            setData(res.data)
+  const [data, setData] = useState([]);
+  const [deleted, setDeleted] = useState(true);
+  useEffect(() => {
+    if (deleted) {
+      setDeleted(false);
+      axios
+        .get("/students")
+        .then((res) => {
+          setData(res.data);
         })
-        .catch((err)=>console.log(err))
+        .catch((err) => console.log(err));
     }
-    }, [deleted])
+  }, [deleted]);
 
-    function handleDelete(id){
-        axios.delete(`/delete/${id}`)
-        .then((res)=>{
-            setDeleted(true)
-        })
-        .catch((err)=> console.log(err))
-    }
+  function handleDelete(id) {
+    axios
+      .delete(`/delete/${id}`)
+      .then((res) => {
+        setDeleted(true);
+      })
+      .catch((err) => console.log(err));
+  }
   return (
-    <div className='container-fluid bg-primary vh-100 vw-100'>
-        <h3>Students</h3>
-        <div className='d-flex justify-content-end'>
-            <Link className='btn btn-success' to='/create'>Add Student</Link>
-        </div>
-        <table>
-            <thead>
+    <div className="container-fluid bg-primary vh-100 vw-100">
+      <h3>Students</h3>
+      <div className="d-flex justify-content-end">
+        <Link className="btn btn-success" to="/create">
+          Add Student
+        </Link>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.length > 0 ? (
+            data.map((student) => {
+              return (
                 <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Age</th>
-                    <th>Gender</th>
-                    <th>Actions</th>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.email}</td>
+                  <td>{student.age}</td>
+                  <td>{student.gender}</td>
+                  <td>
+                    <Link
+                      className="btn mx-2 btn-success"
+                      to={`/read/${student.id}`}
+                    >
+                      Read
+                    </Link>
+                    <Link
+                      className="btn mx-2 btn-success"
+                      to={`/edit/${student.id}`}
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      onClick={() => handleDelete(student.id)}
+                      className="btn mx-2 btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
                 </tr>
-            </thead>
-            <tbody>
-                {
-                    data.map((student)=>{
-                        return (<tr>
-                            <td>{student.id}</td>
-                            <td>{student.name}</td>
-                            <td>{student.email}</td>
-                            <td>{student.age}</td>
-                            <td>{student.gender}</td>
-                            <td>
-                                <Link className='btn mx-2 btn-success' to={`/read/${student.id}`}>Read</Link>
-                                <Link className='btn mx-2 btn-success' to={`/edit/${student.id}`}>Edit</Link>
-                                <button onClick={()=>handleDelete(student.id)} className='btn mx-2 btn-danger'>Delete</button>
-                            </td>
-                        </tr>)
-                    })
-                }
-            </tbody>
-        </table>
+              );
+            })
+          ) : (
+            <tr><td>No Students available</td></tr>
+          )}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
